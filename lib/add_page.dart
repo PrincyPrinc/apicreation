63244a78b75df688512a502336168ddada34efe5
PrincyPrinc <princyprinc1413@e.com>
2024-03-31 21:50:26 +0530

    commitcrud

---
+++ lib/add_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AddToDoPage extends StatefulWidget {
+  final Map? todo;
+  const AddToDoPage({super.key, this.todo});
+
+  @override
+  State<AddToDoPage> createState() => _AddToDoPageState();
+}
+
+class _AddToDoPageState extends State<AddToDoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  bool isEdit = false;
+
+  @override
+  void initState() {
+    super.initState();
+    final todo = widget.todo;
+    if (todo != null) {
+      isEdit = true;
+      final title = todo['title'];
+      final description = todo['description'];
+      titleController.text = title;
+      descriptionController.text = description;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          isEdit ? 'Edit Todo' : 'Add Todo',
+        ),
+      ),
+      body: ListView(
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(hintText: 'Title'),
+          ),
+          TextField(
+            controller: descriptionController,
+            decoration: InputDecoration(hintText: 'Description'),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 8,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+              onPressed: isEdit ? updateData : submitData,
+              child: Text(
+                isEdit ? 'update' : 'submit',
+              ))
+        ],
+      ),
+    );
+  }
+
+  Future<void> updateData() async {
+    final todo = widget.todo;
+    if (todo == null) {
+      print('ypu cannot call updated without todo data');
+      return;
+    }
+    final id = todo['_id'];
+    // final isCompleted = todo['is_completed'];
+    final title = titleController.text;
+    final description = descriptionController.text;
+
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.put(uri,
+        body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode == 200) {
+      print('Creation Success');
+      showSuccessMessage('updation success');
+    } else {
+      print('Creation Failed');
+      showSuccessMessage('updation Failed');
+      print(response.body);
+    }
+  }
+
+  Future<void> submitData() async {
+    final title = titleController.text;
+    final description = descriptionController.text;
+
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(uri,
+        body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode == 201) {
+      titleController.text = '';
+      descriptionController.text = '';
+      print('Creation Success');
+      showSuccessMessage('Creation success');
+    } else {
+      print('Creation Failed');
+      showSuccessMessage('Creation Failed');
+      print(response.body);
+    }
+  }
+
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
